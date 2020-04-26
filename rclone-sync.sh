8b47de3188b83b14e6df37f480da7d063f10f8d7
JF Caringi hostname <47096567+MrCaringi@users.noreply.github.com>
2020-04-26 18:32:20 -0500

    pidof issue fix, telegram notification integration when rclone-sync fails

--- rclone-sync.sh
+++ rclone-sync.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-if pidof -o %PPID -x “rclone-sync.sh”; then
-	echo "ERROR: another instance already running"
+echo "===================================================="
+
+if pidof -o %PPID -x rclone; then
+	echo $(date +"%Y%m%d %H:%M:%S")" ERROR: another instance already running"
 	exit 1
 fi
-	echo "SYNC: local to gdrive-santi:backups/"
+	echo $(date +"%Y%m%d %H:%M:%S")" SYNC: local to gdrive-santi:backups/"
 	rclone sync /mnt/iscsi-borg/ gdrive-santi:backups/
-	echo "SYNC: gdrive-santi:backups/ to onedrive-yahoo:borg/"
+	if $? != 0; then
+		bash /home/jfc/scripts/telegram-message.sh "Borg rclone sync" "ERROR during" "rclone sync /mnt/iscsi-borg/ gdrive-santi:backups/" > /dev/null
+	
+	echo $(date +"%Y%m%d %H:%M:%S")" SYNC: gdrive-santi:backups/ to onedrive-yahoo:borg/"
 	rclone sync gdrive-santi:backups/ onedrive-yahoo:borg/
+	if $? != 0; then
+		bash /home/jfc/scripts/telegram-message.sh "Borg rclone sync" "ERROR during" "rclone sync gdrive-santi:backups/ onedrive-yahoo:borg/" > /dev/null
 exit
