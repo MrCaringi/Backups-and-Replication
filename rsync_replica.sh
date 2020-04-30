7e6454cb8c6413e67ba908b96e87d1736e36a1cb
JF Caringi hostname <47096567+MrCaringi@users.noreply.github.com>
2020-04-29 20:29:52 -0500

    aplha version 11

--- rsync_replica.sh
+++ rsync_replica.sh
@@ -23,7 +23,8 @@
 #   SSHPASS=passphrase      - ssh password in order to shutdown the remote when finish
 #   RSYNCUSER=rsync-user    - rsync user
 #   RSYNCPASS=passphrase    - rsync password
-#   HOST=NAS                - hostname indicated in .ssh/config 
+#   HOST=NAS                - hostname indicated in .ssh/config
+#   IPRSYNC=10.0.0.15       - Rsync server/NAS IP
 #   IP=10.0.0.0             - Network Address (not the remote IP address) for WOL command
 #   MAC=00:00:00:00:00      - remote MAC address for WOL command
 #   MIN=5                   - Minutes to wait after WOL
@@ -49,7 +50,7 @@ sleep ${MIN}m
 echo $(date +%Y%m%d-%H%M)" Starting Rsync folders"
 bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "Starting Rsync folders"
 
-sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/iscsi-borg $RSYNCUSER@$IP::borg
+sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/iscsi-borg $RSYNCUSER@$IPRSYNC::borg
 if $? != 0; then
 	echo $(date +%Y%m%d-%H%M)" ERROR RSYNC /mnt/iscsi-borg"
     bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during RSYNC" "/mnt/iscsi-borg"
@@ -58,7 +59,7 @@ if $? != 0; then
     exit 1
 fi
 
-sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-Music $RSYNCUSER@$IP::music
+sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-Music $RSYNCUSER@$IPRSYNC::music
 if $? != 0; then
 	echo $(date +%Y%m%d-%H%M)" ERROR RSYNC /mnt/nostromo-Music"
     bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during RSYNC" "/mnt/nostromo-Music"
@@ -67,7 +68,7 @@ if $? != 0; then
     exit 1
 fi
 
-sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-photo $RSYNCUSER@$IP::photo
+sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-photo $RSYNCUSER@$IPRSYNC::photo
 if $? != 0; then
 	echo $(date +%Y%m%d-%H%M)" ERROR RSYNC /mnt/nostromo-photo"
     bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during RSYNC" "/mnt/nostromo-photo"
@@ -76,7 +77,7 @@ if $? != 0; then
     exit 1
 fi
 
-sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-video $RSYNCUSER@$IP::video
+sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-video $RSYNCUSER@$IPRSYNC::video
 if $? != 0; then
 	echo $(date +%Y%m%d-%H%M)" ERROR RSYNC /mnt/nostromo-video"
     bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during RSYNC" "/mnt/nostromo-video"
@@ -85,11 +86,10 @@ if $? != 0; then
     exit 1
 fi
 
-echo $(date +%Y%m%d-%H%M)" ERROR during WOL of device $IP $MAC"
-bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during WOL" "of $IP"
-
 ##   Turning off remote device
-sleep 20
+echo $(date +%Y%m%d-%H%M)" INFO RSYNC successfully done on $IP"
+bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "RSYNC successfully done" "of $IP"
+sleep 5
 echo $SSHPASS | ssh -tt quiltra "shutdown -h now"
-
+sleep 5
 exit 0
