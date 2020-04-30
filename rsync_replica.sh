ea50fdc177b8c64257bd1d464e8fb65f2a282481
JF Caringi hostname <47096567+MrCaringi@users.noreply.github.com>
2020-04-29 20:02:34 -0500

    aplha version 8

--- rsync_replica.sh
+++ rsync_replica.sh
@@ -20,6 +20,8 @@
 ##	RSYNC CONFIGURATION
 #   It must include:
 #   SSHPASS=passphrase      - ssh password in order to shutdown the remote when finish
+#   RSYNCUSER=rsync-user    - rsync user
+#   RSYNCPASS=passphrase    - rsync password
 #   HOST=NAS                - hostname indicated in .ssh/config 
 #   IP=10.0.0.0             - Network Address (not the remote IP address) for WOL command
 #   MAC=00:00:00:00:00      - remote MAC address for WOL command
@@ -52,9 +54,40 @@ sleep ${MIN}m
 echo $(date +%Y%m%d-%H%M)" Starting Rsync folders"
 bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "Starting Rsync folders"
 
-##   Turning off remote device
+sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/iscsi-borg $RSYNCUSER@$IP::borg
+if $? != 0; then
+	echo $(date +%Y%m%d-%H%M)" ERROR RSYNC /mnt/iscsi-borg"
+    bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during RSYNC" "/mnt/iscsi-borg"
+    exit 1
+fi
 
-echo $SSHPASS | ssh -tt quiltra "sudo ps -A"
+sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-Music $RSYNCUSER@$IP::music
+if $? != 0; then
+	echo $(date +%Y%m%d-%H%M)" ERROR RSYNC /mnt/nostromo-Music"
+    bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during RSYNC" "/mnt/nostromo-Music"
+    exit 1
+fi
+
+sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-photo $RSYNCUSER@$IP::photo
+if $? != 0; then
+	echo $(date +%Y%m%d-%H%M)" ERROR RSYNC /mnt/nostromo-photo"
+    bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during RSYNC" "/mnt/nostromo-photo"
+    exit 1
+fi
+
+sshpass -p $RSYNCPASS rsync -aq --append-verify /mnt/nostromo-video $RSYNCUSER@$IP::video
+if $? != 0; then
+	echo $(date +%Y%m%d-%H%M)" ERROR RSYNC /mnt/nostromo-video"
+    bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during RSYNC" "/mnt/nostromo-video"
+    exit 1
+fi
+
+echo $(date +%Y%m%d-%H%M)" ERROR during WOL of device $IP $MAC"
+bash /home/jfc/scripts/telegram-message.sh "RSYNC Replica" "ERROR during WOL" "of $IP"
+
+##   Turning off remote device
+sleep 20
+echo $SSHPASS | ssh -tt quiltra "shutdown -h now"
 
 exit
 
