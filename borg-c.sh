0bae56d249e41264d59f97dbf70c26bec0ffbdc3
JF Caringi hostname <47096567+MrCaringi@users.noreply.github.com>
2020-07-30 17:03:00 -0500

    alpha version

--- borg-c.sh
+++ borg-c.sh
@@ -24,7 +24,7 @@ REP_DIR=${1}
 
 #################################
 #   CONFIG FILE
-#   /home/jfc/scripts/borg.conf
+#   example     /home/jfc/scripts/borg.conf
 #   this should be like this
 #
 #   PASSPHRASE='password'
@@ -33,11 +33,6 @@ REP_DIR=${1}
 #   $(date +"%Y%m%d")"   
 . /home/jfc/scripts/borg.conf
 
-#testing
-echo $REP_DIR
-echo $BORG_PASSPHRASE
-exit 0
-
 echo "=============================================================================="
 
 # Setting this, so you won't be asked for your repository passphrase:
@@ -45,70 +40,49 @@ export BORG_PASSPHRASE=${PASSPHRASE}
 
 # some helpers and error handling:
 info() { printf "\n%s %s\n\n" "$( date )" "$*"; }
-trap 'echo $( date ) Backup interrupted ; exit 2' INT TERM
+trap 'echo $( date ) Check interrupted ; exit 2' INT TERM
 
-info "Starting backup"
+info "Starting Repository Check"
 echo $(date +%Y%m%d-%H%M)" Starting backup of ${TITLE}"
-bash /home/jfc/scripts/telegram-message.sh "Borg Backup" "Repo: #${TITLE}" "Starting backup" > /dev/null
 
-# Backup the most important directories into an archive named after
-# the machine this script is currently running on:
+##  Getting the list of repositories
+REP_LIST=`cat $REP_DIR`
 
-##  Running the backup and capturing the output to a variable
-#   log variable will be used to sent the log via telegram
-log_create=`borg create --stats --list --filter=E --compression auto,lzma,9 ${FULLREP} ${ORI} 2>&1`
-log_create=`borg create --stats --list --filter=E --compression auto,lzma,9 ${FULLREP} ${ORI} 2>&1`
+for i in $REP_LIST
+do
+    echo "================================================"
+    REPO=${i##*/}
+    echo $(date +%Y%m%d-%H%M)" Starting Check of $REPO"
+    bash /home/jfc/scripts/telegram-message.sh "Borg Check" "Repo: #${REPO}" "Starting Check of repository" > /dev/null
+    
+    #   The Magic goes here
+    log_check=`borg check -v --verify-data --show-rc $i`
+    exit=$?
 
-backup_exit=$?
+    if [ ${exit} -eq 0 ]; then
+        info "Check of ${REPO} finished successfully"
+        echo $(date +%Y%m%d-%H%M)" Check of ${REPO} finished successfully"
+        bash /home/jfc/scripts/telegram-message.sh "Borg Check" "Repo: #${REPO}" "Check finished #successfully" > /dev/null
+    else
+        info "Check of ${REPO} finished with errors"
+        echo $(date +%Y%m%d-%H%M)" Check of ${REPO} finished with errors"
+        bash /home/jfc/scripts/telegram-message.sh "Borg Check" "Repo: #${REPO}" "Check finished with #errors" > /dev/null
+    fi
 
-info "Pruning repository"
-echo $(date +%Y%m%d-%H%M)" Pruning repository of ${TITLE}"
-bash /home/jfc/scripts/telegram-message.sh "Borg Backup" "Repo: #${TITLE}" "Pruning repository" > /dev/null
+    ##  Sending log to Telegram
+    #   Building the log file
+        rand=$((1000 + RANDOM % 8500))
+        echo "========== BORG CHECK" >> borg-log_${rand}.log
+        echo "$log_check" >> borg-log_${rand}.log
+        echo >> borg-log_${rand}.log
+        echo $(date +"%Y%m%d %HH%MM%SS") >> borg-log_${rand}.log
+        echo "========== END" >> borg-log_${rand}.log
+        #   Sending the File to Telegram
+        bash /home/jfc/scripts/telegram-message-file.sh "Repo: #${REPO}" "Borg Check Log File" borg-log_${rand}.log > /dev/null
+        #   Flushing & Deleting the file
+        cat borg-log_${rand}.log
+        rm borg-log_${rand}.log
 
-###     PRUNE
+done
 
-if [ $backup_exit -eq 0 ]; then
-    log_prune=`borg prune -v -s --list --keep-daily=$D --keep-weekly=$W --keep-monthly=$M $REP 2>&1`
-    prune_exit=$?
-else
-    echo $(date +%Y%m%d-%H%M)" Backup not completed, skip Pruning of ${TITLE}"    
-fi
-
-# use highest exit code as global exit code
-global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
-
-if [ ${global_exit} -eq 0 ]; then
-    info "Backup and Prune finished successfully"
-    echo $(date +%Y%m%d-%H%M)" Backup and Prune finished successfully"
-	bash /home/jfc/scripts/telegram-message.sh "Borg Backup" "Repo: #${TITLE}" "Backup and Prune finished #successfully" > /dev/null
-elif [ ${global_exit} -eq 1 ]; then
-    info "Backup and/or Prune finished with warnings"
-    echo $(date +%Y%m%d-%H%M)" Backup and/or Prune finished with warnings"
-	bash /home/jfc/scripts/telegram-message.sh "Borg Backup" "Repo: #${TITLE}" "Backup and/or Prune finished with #warnings" > /dev/null
-else
-    info "Backup and/or Prune finished with errors"
-    echo $(date +%Y%m%d-%H%M)" Backup and/or Prune finished with errors"
-	bash /home/jfc/scripts/telegram-message.sh "Borg Backup" "Repo: #${TITLE}" "Backup and/or Prune finished with #errors" > /dev/null
-fi
-
-##  Sending log to Telegram
-#   Building the log file
-rand=$((1000 + RANDOM % 8500))
-echo "========== BORG CREATE" >> borg-log_${rand}.log
-echo "$log_create" >> borg-log_${rand}.log
-echo >> borg-log_${rand}.log
-echo "========== BORG PRUNE" >> borg-log_${rand}.log
-echo $(date +"%Y%m%d %HH%MM%SS") >> borg-log_${rand}.log
-echo >> borg-log_${rand}.log
-echo "$log_prune" >> borg-log_${rand}.log
-echo "========== END" >> borg-log_${rand}.log
-echo $(date +"%Y%m%d %HH%MM%SS") >> borg-log_${rand}.log
-
-#   Sending the File to Telegram
-bash /home/jfc/scripts/telegram-message-file.sh "Repo: #${TITLE}" "Log File" borg-log_${rand}.log > /dev/null
-
-#   Flushing & Deleting the file
-cat borg-log_${rand}.log
-rm borg-log_${rand}.log
-
-exit ${global_exit}
+exit ${exit}
